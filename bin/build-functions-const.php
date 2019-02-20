jasny/improved-php-iterable

Fixed build-functions-const script Minor README changes [skip ci]

bin/build-functions-const.php
@@ -2,23 +2,24 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+const FUNCTION_PREFIX = 'iterable_';
+
 $userFunctions = get_defined_functions()['user'];
 $functions = [];
 
 foreach ($userFunctions as $function) {
-    if (stripos($function, 'ipl\\iterable_') === 0) {
+    if (stripos($function, 'improved\\' . FUNCTION_PREFIX) === 0) {
         $functions[] = $function;
     }
 }
 
-$code = ["<?php\n\n// phpcs:ignoreFile\n\ndeclare(strict_types=1);\n\nnamespace Ipl;\n"];
+$code = ["<?php\n\n/** @ignoreFile */\n// phpcs:ignoreFile\n\ndeclare(strict_types=1);\n\nnamespace Improved;\n"];
 
 foreach ($functions as $function) {
     $fnName = substr($function, 4);
-    $code[] = sprintf('const %1$s = "Ipl\\\\%1$s";', $fnName);
+    $code[] = "/** @internal */\n" . sprintf('const %1$s = "Improved\\\\%1$s";', $fnName);
 }
 
 file_put_contents(dirname(__DIR__) . '/src/functions-const.php', join("\n", $code) . "\n");
 
 echo "Created functions-const.php\n";
-

jasny/improved-php-function

bin/build-functions-const.php
@@ -2,11 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+const FUNCTION_PREFIX = 'function_';
+
 $userFunctions = get_defined_functions()['user'];
 $functions = [];
 
 foreach ($userFunctions as $function) {
-    if (stripos($function, 'improved\\function_') === 0) {
+    if (stripos($function, 'improved\\' . FUNCTION_PREFIX) === 0) {
         $functions[] = $function;
     }
 }
@@ -15,7 +17,7 @@ $code = ["<?php\n\n/** @ignoreFile */\n// phpcs:ignoreFile\n\ndeclare(strict_typ
 
 foreach ($functions as $function) {
     $fnName = substr($function, 4);
-    $code[] = "/** @ignore */\n" . sprintf('const %1$s = "Improved\\\\%1$s";', $fnName);
+    $code[] = "/** @internal */\n" . sprintf('const %1$s = "Improved\\\\%1$s";', $fnName);
 }
 
 file_put_contents(dirname(__DIR__) . '/src/functions-const.php', join("\n", $code) . "\n");